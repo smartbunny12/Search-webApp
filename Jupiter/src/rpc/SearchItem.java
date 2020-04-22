866e89011fac1269f689018877b37ccc1ddf0666
smartbunny12 <xuefanqi12@gmail.com>
2020-04-21 20:58:40 -0700

    helperClass

--- Jupiter/src/rpc/SearchItem.java
+++ Jupiter/src/rpc/SearchItem.java
@@ -1,11 +1,16 @@
 package rpc;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class SearchItem
@@ -27,7 +32,21 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String username = "";
+		if (request.getParameter("username") != null) {
+			username = request.getParameter("username");
+		}
+		JSONArray array = new JSONArray();
+		
+		try {
+			array.put(new JSONObject().put("username", username));
+			array.put(new JSONObject().put("username", "Ma"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		RpcHelper.writeJsonArray(response, array);
+
+		
 	}
 
 	/**
